d0454d7909ca3afd5b776e51c902b4741f57c978
Dominik Krajcer <dom.kraj@gmail.com>
2026-03-11 15:27:03 +0100

    refactor(button): move fields after constructors

--- docudart/components/button.dart
+++ docudart/components/button.dart
@@ -2,10 +2,6 @@ import 'package:docudart/docudart.dart';
 
 /// A clickable button component.
 class Button extends StatelessComponent {
-  final String text;
-  final String href;
-  final String classes;
-
   const Button({
     required this.text,
     required this.href,
@@ -15,6 +11,10 @@ class Button extends StatelessComponent {
 
   const Button.primary({required this.text, required this.href, super.key})
     : classes = 'button button-primary';
+
+  final String text;
+  final String href;
+  final String classes;
 
   @override
   Component build(BuildContext context) {
